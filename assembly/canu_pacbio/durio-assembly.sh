f994eee6e89077eb0ed42eeb3d5927c3235731cc
projectoriented <savantmei@gmail.com>
2020-03-30 23:34:37 +0200

    update-v1

--- assembly/canu_pacbio/durio-assembly.sh
+++ assembly/canu_pacbio/durio-assembly.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 canu -p durioz -d . genomeSize=500m -pacbio-raw ~/5_Tean_Teh_2017/pacbio_data/SRR6037732_scaffold_10.fq.gz
+
+canu -trim \
+    -p durioz -d . \
+    genomeSize=500m \
+    -pacbio-corrected .durioz.correctedReads.fasta.gz
