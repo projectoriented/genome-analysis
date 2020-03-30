bc5a4849406d535f80b3fd4f7a01d266b7024875
projectoriented <savantmei@gmail.com>
2020-03-31 00:31:48 +0200

    update

--- assembly/canu_pacbio/durio-assembly.sh
+++ assembly/canu_pacbio/durio-assembly.sh
@@ -1,26 +1,39 @@
-#!/bin/bash
+#!/bin/bash 
 
-canu -p durioz -d . genomeSize=500m -pacbio-raw ~/5_Tean_Teh_2017/pacbio_data/SRR6037732_scaffold_10.fq.gz
+
+# Load modules
+module load bioinfo-tools
+module load canu
+
+canu -correct \
+    -p durioz -d . \
+    genomeSize=800m \
+    executiveThreads=2 \
+    -pacbio-raw ~/5_Tean_Teh_2017/pacbio_data/SRR6037732_scaffold_10.fq.gz
 
 canu -trim \
     -p durioz -d . \
-    genomeSize=500m \
-    -pacbio-corrected .durioz.correctedReads.fasta.gz
+    genomeSize=800m \
+    executiveThreads=2 \
+    -pacbio-corrected ./durioz.correctedReads.fasta.gz
 
 canu -assemble \
   -p durioz -d durioz-erate-0.030 \
-  genomeSize=500m \
+  genomeSize=800m \
+  executiveThreads=2 \
   correctedErrorRate=0.030 \
-  -pacbio-corrected .durioz.trimmedReads.fasta.gz
+  -pacbio-corrected ./durioz.trimmedReads.fasta.gz
 
 canu -assemble \
   -p durioz -d durioz-erate-0.045 \
-  genomeSize=500m \
+  genomeSize=800m \
+  executiveThreads=2 \
   correctedErrorRate=0.045 \
-  -pacbio-corrected .durioz.trimmedReads.fasta.gz
+  -pacbio-corrected ./durioz.trimmedReads.fasta.gz
 
 canu -assemble \
   -p durioz -d durioz-erate-0.055 \
-  genomeSize=500m \
+  genomeSize=800m \
+  executiveThreads=2 \
   correctedErrorRate=0.055 \
-  -pacbio-corrected .durioz.trimmedReads.fasta.gz
+  -pacbio-corrected ./durioz.trimmedReads.fasta.gz
