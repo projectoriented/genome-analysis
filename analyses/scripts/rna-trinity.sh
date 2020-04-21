e3997b7d20b7efb505ca40dd3280f265463fcf6f
projectoriented <savantmei@gmail.com>
2020-04-21 22:44:18 +0200

    rna assembly

--- analyses/scripts/rna-trinity.sh
+++ analyses/scripts/rna-trinity.sh
@@ -3,24 +3,19 @@
 #SBATCH -A g2020008
 #SBATCH -p core
 #SBATCH -n 4
-#SBATCH -t 8:00:00
-#SBATCH -J rna-trinity
+#SBATCH -t 15:00:00
+#SBATCH -J rna_trinity
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user meifang.wu.7427@student.uu.se
 
 module load bioinfo-tools
 module load trinity
 
-'
-Trinity --seqType fq \
-    --left ~/genome-analysis/analyses/data/raw_data/transcriptome/trimmed/*10.1.* \
-    --right ~/genome-analysis/analyses/data/raw_data/transcriptome/trimmed/*10.2.* \
-    --CPU 8 \
-    --max_memory 25G \
-    --output ~/genome-analysis/analyses/02_genome_assembly/rna_trinity
-'
-
-Trinity --genome_guided_bam ~genome-analysis/analyses/02_genome_assembly/rna_tophat/*.bam \
+Trinity \
+    --genome_guided_bam /home/mei/genome-analysis/analyses/02_genome_assembly/rna_star/durioz-star.Aligned.sortedByCoord.out.bam \
+    --genome_guided_max_intron 10000 \
     --max_memory 25G \
     --CPU 8 \
+    --full_cleanup \
+    --output /home/mei/genome-analysis/analyses/02_genome_assembly/rna_trinity
     
