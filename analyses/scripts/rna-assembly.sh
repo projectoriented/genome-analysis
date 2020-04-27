aab1069ad6c5e128a26ee7130b5adba275af29d1
projectoriented <savantmei@gmail.com>
2020-04-27 15:48:23 +0200

    FINAL VER

--- analyses/scripts/rna-assembly.sh
+++ analyses/scripts/rna-assembly.sh
@@ -15,13 +15,13 @@ module load trinity
 dir=~/genome-analysis/analyses/02_genome_assembly
 data=~/genome-analysis/analyses/data/raw_data/transcriptome/trimmed
 
-files=$(ls -d /home/mei/genome-analysis/analyses/data/raw_data/transcriptome/trimmed/*_scaffold_10.*.fastq.gz )
+files=( $(ls -d /home/mei/genome-analysis/analyses/data/raw_data/transcriptome/trimmed/*_scaffold_10.*.fastq.gz) )
 
-for i in $files
+for (( i=0; i<${#files[@]}; i+=2 ))
 do
-    p1=$(echo $i | grep '10.1.')
-    p2=$(echo $i | grep '10.2.')
-    sra=$(echo $p1 | sed s'/.*ed\///; s/_sc.*//')
+    p1=$(echo ${files[i]})
+    p2=$(echo ${files[i+1]})
+    sra=$(echo $p1 | grep -oE '\bSRR[0-9]{7}')
 
     #~/genome-analysis/analyses/scripts/rna-index-star.sh
 
@@ -39,10 +39,10 @@ do
 	--genome_guided_max_intron 10000 \
 	--max_memory 25G \
 	--CPU 8 \
-	--full_cleanup \ # this doesn't work.
+	--full_cleanup \ 
 	--output $dir/rna_trinity/$sra-Trinity
 
 # since full_cleanup doesn't work, manual cleanup is needed.
-    ls $dir/rna_trinity/* -d | grep -v '.fasta' | xargs rm -rf
+    ls $dir/rna_trinity/*/* -d | grep -v '.fasta' | xargs rm -rf
 
 done
