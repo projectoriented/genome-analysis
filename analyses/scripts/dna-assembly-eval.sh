325df466669caa24a87b445244b33c5d61fb930e
projectoriented <savantmei@gmail.com>
2020-05-13 00:43:25 +0200

    update

--- analyses/scripts/dna-assembly-eval.sh
+++ analyses/scripts/dna-assembly-eval.sh
@@ -8,4 +8,11 @@ dir=/home/mei/genome-analysis/analyses
 quast.py $dir/02_genome_assembly/pilon_assembly/durioz.pilon.fasta \
     -R $dir/data/NSDW01000010.1.fasta \
     -t 8 \
-    -o $dir/02_genome_assembly/quast_evaluation
+    -o $dir/02_genome_assembly/quast_evaluation/genome
+
+quast.py $dir/02_genome_assembly/rna_trinity/*fasta \
+    -R $dir/data/NSDW01000010.1.fasta \
+    -t 8 \
+    -o $dir/02_genome_assembly/quast_evaluation/transcriptome
+
+
