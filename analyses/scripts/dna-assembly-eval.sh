f917eb1713fd8e43018dc77f2d2aa6d0020decc7
projectoriented <savantmei@gmail.com>
2020-05-13 13:29:58 +0200

    update

--- analyses/scripts/dna-assembly-eval.sh
+++ analyses/scripts/dna-assembly-eval.sh
@@ -5,14 +5,12 @@ module load quast
 
 dir=/home/mei/genome-analysis/analyses
 
+# De novo genome assembly QC \
 quast.py $dir/02_genome_assembly/pilon_assembly/durioz.pilon.fasta \
     -R $dir/data/NSDW01000010.1.fasta \
     -t 8 \
     -o $dir/02_genome_assembly/quast_evaluation/genome
 
-quast.py $dir/02_genome_assembly/rna_trinity/*fasta \
-    -R $dir/data/NSDW01000010.1.fasta \
-    -t 8 \
-    -o $dir/02_genome_assembly/quast_evaluation/transcriptome
+# De novo transcriptome assembly QC \
 
 
